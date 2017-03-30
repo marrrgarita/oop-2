d11e6d473cc0800b5836692cf6760e41b429504b
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-30 19:36:08 -0400

    Finish regular exercise

--- booklending.rb
+++ booklending.rb
@@ -16,6 +16,11 @@ class Book
   end
 
   def self.overdue_books
+    @@on_loan.each do |book|
+      if book.due_date < Time.now
+        puts book
+      end
+      end
   end
 
   def self.browse
@@ -42,21 +47,38 @@ class Book
 
   def borrow
     if lent_out? == true
+      puts "Sorry #{title} is already borrowed"
+      @@on_shelf.include?(@title)
+    elsif lent_out? != true
       @due_date = Book.current_due_date
       @@on_shelf.delete(self)
       @@on_loan.push(self)
       "You have borrowed #{@title} and it is due back #{@due_date}"
-    elsif lent_out? != true
-      puts "Sorry #{title} is already borrowed"
-      @@on_shelf.include?(@title)
     end
   end
 
   def return_to_library
+    if lent_out? != true
+      puts "This book is not loaned out"
+    else
+      @due_date = nil
+      @@on_loan.delete(self)
+      @@on_shelf.push(self)
+      "Thank you for returning #{@title}"
+    end
   end
 
   def lent_out?
-    @@on_loan.include?(@title)
+    @@on_loan.include?(self)
+  end
+
+  def renew
+    @due_date = Time.now + (60 * 60 * 24 * 7)
+    "You have renewed #{@title}, it's new due date is #{@due_date}"
   end
 
 end
+
+sister_outsider = Book.create("Sister Outsider", "Audre Lorde", "9781515905431")
+aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", "9780896081307")
+if_they_come = Book.create("If They Come in the Morning", "Angela Y. Davis", "0893880221")
