5ccc2358899258033c119d7a4441ef2bef41e95e
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-30 19:58:52 -0400

    create shell instance and class methods

--- zombie.rb
+++ zombie.rb
@@ -1,2 +1,47 @@
 class Zombie
+  @@horde = [] #will contain array of zombies
+  @@plague_level = 10 #will increase
+  @@max_speed = 5
+  @@max_strength = 8
+  @@default_speed = 1
+  @@defaul_strength = 3
+
+################ CLASS METHODS
+  def self.all
+
+  end
+
+  def self.new_day
+
+  end
+
+  def self.some_die_off
+
+  end
+
+  def self.spawn
+
+  end
+
+  def self.increase_plague_level
+
+  end
+################ INSTANCE METHODS
+
+  def initalize
+
+  end
+
+  def encounter
+
+  end
+
+  def outrun_zombie?
+
+  end
+
+  def survive_attack?
+
+  end
+
 end
