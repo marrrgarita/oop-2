7ddd366a41ae3f712e66d1461223d74105a84d92
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-31 14:38:00 -0400

    class method create to make new vampires and add them to the coven

--- vampire.rb
+++ vampire.rb
@@ -1,11 +1,20 @@
 class Vampire
 
+  @@coven
+
+  def self.create(name, age)
+    new_vampire = new(name, age)
+    @@coven << new_vampire
+    new_vampire
+  end
+
   def initialize(name, age)
     @name = name
     @age = age
     @in_coffin = false
     @drank_blood_today = false
   end
+
 
 
 end
