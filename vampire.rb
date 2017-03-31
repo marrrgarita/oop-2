b3f8bc93910a95a3b9e78d0d85385b73ba72b37b
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-31 14:43:48 -0400

    add drink_blood method

--- vampire.rb
+++ vampire.rb
@@ -1,6 +1,8 @@
 class Vampire
 
-  @@coven
+  @@coven = []
+
+#####CLASS METHODS
 
   def self.create(name, age)
     new_vampire = new(name, age)
@@ -8,12 +10,23 @@ class Vampire
     new_vampire
   end
 
+  def self.coven
+    @@coven
+  end
+
+######INSTANCE METHODS
+
   def initialize(name, age)
     @name = name
     @age = age
     @in_coffin = false
     @drank_blood_today = false
   end
+
+  def drink_blood
+    @drank_blood_today = true
+  end
+  
 
 
 
